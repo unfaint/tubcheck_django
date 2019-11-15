1a751ae4dd3a2611a623c9067044c94fa3940a96
Vladimir Kruglov <vladimir.kruglov@gmail.com>
2019-11-15 12:55:43 +0300

    CHANGE: home page with form

--- tubcheck/oneimage/views.py
+++ tubcheck/oneimage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    pass
+    return render(request, 'oneimage/home_page.html')
--- tubcheck/tubcheck/urls.py
+++ tubcheck/tubcheck/urls.py
@@ -19,5 +19,5 @@ from oneimage import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_page)
+    path('', views.home_page),
 ]
